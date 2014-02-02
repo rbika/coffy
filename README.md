f5c12a5cfe320bfc8795a04830418817e7b1f4f8
Rafael Bika <rbmbika@gmail.com>
2014-02-02 10:04:14 -0200

    Coffy 2.0

--- README.md
+++ README.md
@@ -1,39 +1,29 @@
-Coffee's Guy - coffy
+Coffy 2.0
 ====================
-Coffee's Guy is an nice office tool, that makes a lot easier the decision of who is going to make the coffee.
-Just give it a list of names and it will choose one.
+Coffy is an nice office tool that makes a lot easier the decision of who is
+going to make the coffee. Just give it a list of names and it will choose one.
 
 Usage
 -----
-**coffy** [options] action
+**coffy** [names]
 
-Supported actions
------------------
-	name name [name ...]    list of names to be chosen
-	help                    display this help message
-	version                 display version
-	stats                   display usage statistics
-    remove name [name ...]  remove stats for the listed names  
-	reset                   reset statistics
-
-Options
--------
-    -x, --no-stats     does not compute statistics
+How it works
+-----
 
 Installation
 ------------
 For OS X users:  
-`sudo curl -o /opt/local/bin/coffy https://raw.github.com/rbika/coffy/master/coffy.py`  
-`sudo chmod +x /opt/local/bin/coffy`  
+`sudo curl -o /opt/local/bin/coffy https://raw.github.com/rbika/coffy/master/coffy.py`
+`sudo chmod +x /opt/local/bin/coffy`
 
 For Linux users:  
-`sudo wget -O /usr/local/bin/coffy https://raw.github.com/rbika/coffy/master/coffy.py`  
-`sudo chmod +x /usr/local/bin/coffy`  
+`sudo wget -O /usr/local/bin/coffy https://raw.github.com/rbika/coffy/master/coffy.py`
+`sudo chmod +x /usr/local/bin/coffy`
 
 Example
 -------
-running:  
-`coffy bika rafa wlad dudu mattar`  
+running:
+`coffy bika rafa wlad dudu mattar`
 
 You'll get for example:
 
--- coffy.py
+++ coffy.py
@@ -1,49 +1,136 @@
-# Python.
-from random import choice
-from datetime import datetime
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-# Pymongo.
-from pymongo import Connection
+# Name: Coffy
+# Version: 2.0
+# Author: Rafael Bika
 
-# Bottle.
-from bottle import route, request, run, template, redirect
+import os
+import sys
+import random
+import sqlite3
+import datetime
 
-db = Connection("mongodb://localhost", safe=True).coffy
+# Globals. {{{
+_DATABASE_PATH = os.path.expanduser('~') + '/.coffy.db'
 
+_ERROR1 = 'Enter at least one name.'
 
-@route('/')
-def home():
-    form_names = request.query.getlist('names')
-    context = {'result': ''}
-
-    if len(form_names) > 1:
-        now = datetime.now()
-        chosen = choice(form_names)
-        doc1 = {'names': form_names, 'created': now, 'chosen': chosen}
-        doc2 = {'$inc': {'participated': 1}}
-        doc3 = {'$inc': {'chosen': 1}}
-
-        db.coffy_log.insert(doc1)
-        db.coffy_user.update({'name': {'$in': form_names}}, doc2, multi=True)
-        db.coffy_user.update({'name': chosen}, doc3)
-
-        context['result'] = chosen
-
-    context['names'] = [x['name'] for x in db.coffy_user.find()]
-
-    return template('home', **context)
+_RESULT = u'''
+      ((((
+     ((((
+      ))))
+   _ .---.
+  | |`---'|      %s
+   \|     |      %s
+   : .___, :
+    `-----´
+'''
 
 
-@route('/add')
-def add_name():
-    name = request.query.get('name', '')
-
-    if name:
-        doc = {'name': name, 'participated': 0, 'chosen': 0}
-
-        db.coffy_user.insert(doc)
-
-        redirect('/')
+# }}}
 
 
-run(host='localhost', port=8080, debug=True, reloader=True)
+class User(object):
+    def __init__(self, name, chosen, participated, ratio):
+        self.name = name
+        self.chosen = chosen
+        self.participated = participated
+        self.ratio = ratio
+
+    def print_stats(self):
+        data = (self.name, self.chosen, self.participated, self.ratio)
+
+        print('%s - chosen: %d, participated: %d, %.2f' % data)
+
+
+class Coffy(object):
+    def __init__(self, names):
+        self.participants = []
+        self.date = datetime.datetime.now()
+        self.chosen = ''
+
+        cur.execute('''CREATE TABLE IF NOT EXISTS Users (id TEXT PRIMARY KEY,
+            chosen INT, participated INT, ratio FLOAT)''')
+
+        cur.execute('''CREATE TABLE IF NOT EXISTS Log (id INT PRIMARY KEY,
+            chosen TEXT, participants TEXT, date DATETIME)''')
+
+        for name in names:
+            query = u'SELECT * FROM Users WHERE Id=?'
+            data = cur.execute(query, (name,)).fetchone()
+
+            if not data:
+                data = (name, 0, 0, 0)
+
+            self.participants.append(User(*data))
+
+    def choose(self):
+        ratio = 1
+        lst = []
+
+        for user in self.participants:
+            if user.ratio == ratio:
+                lst.append(user)
+
+            elif user.ratio < ratio:
+                ratio = user.ratio
+                lst = [user]
+
+        if len(lst) == 1:
+            self.chosen = lst[0].name
+
+        elif len(lst) > 1:
+            self.chosen = random.choice(lst).name
+
+        for user in self.participants:
+            if user.name == self.chosen:
+                user.chosen += 1
+
+            user.participated += 1
+            user.ratio = float(user.chosen) / float(user.participated)
+
+    def print_stats(self):
+        for user in self.participants:
+            user.print_stats()
+
+    def print_result(self):
+        print(_RESULT % (self.chosen, self.date.strftime('%a, %I:%M %p')))
+
+    def save(self):
+        val1 = cur.execute('SELECT COUNT (id) FROM Log').fetchone()[0] + 1
+        val2 = ','.join([x.name for x in self.participants])
+
+        for user in self.participants:
+            data = (user.chosen, user.participated, user.ratio, user.name)
+
+            cur.execute('INSERT OR IGNORE INTO Users VALUES (?, 0, 0, 0.0)',
+                (user.name,))
+
+            cur.execute('''UPDATE Users SET chosen=?, participated=?, ratio=?
+                WHERE id=?''', data)
+
+        cur.execute('INSERT INTO Log VALUES (?, ?, ?, ?)',
+            (val1, self.chosen, val2, self.date))
+
+        con.commit()
+
+
+if __name__ == '__main__':
+    con = sqlite3.connect(_DATABASE_PATH)
+
+    with con:
+        cur = con.cursor()
+
+        if sys.argv[1:]:
+            coffy = Coffy(sys.argv[1:])
+
+            coffy.print_stats()
+            coffy.choose()
+            coffy.print_result()
+            coffy.save()
+
+        else:
+            print _ERROR1
+
+# vim:foldmethod=marker
